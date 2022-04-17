49a9d184aaa8b571c9681e401ce91f11f06f3428
shravyarao889 <96167495+shravyarao889@users.noreply.github.com>
2022-04-17 19:05:49 +0530

    navbar added

---
+++ AMS/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+import { AllRoutes } from './components/Routes'
+function App() {
+  
+  return (
+    <div className="App">
+     <AllRoutes/>
+    </div>
+  )
+}
+
+export default App
---
+++ AMS/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { useNavigate } from "react-router-dom"
+
+export const Navbar = () => {
+    const navigate = useNavigate();
+    return (
+        <>
+        <button onClick={()=>{
+            
+        }}>Login/SignUp</button>
+        
+        </>
+    )
+}
